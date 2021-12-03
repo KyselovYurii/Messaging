1ee6befe251ef258b2bfc07912e2a361cf7af3e9
KyselovYurii <kyselov.yurii@gmail.com>
2021-12-03 13:35:51 +0200

    Add server and client projects

---
+++ Messaging/MsmqClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Messaging;
+using System.Threading;
+
+namespace MsmqClient
+{
+    public class Program
+    {
+        private const string MessageQueueName = @".\private$\TestMessageQueue";
+        private const string FolderPath = @"C:\Users\Yurii_Kyselov\ClientFolder";
+        private const int BufferSize = 2 * 1024 * 1024;
+
+        private static MessageQueue _messageQueue;
+
+        private static void Main(string[] args)
+        {
+            _messageQueue = MessageQueue.Exists(MessageQueueName) ? new MessageQueue(MessageQueueName) : MessageQueue.Create(MessageQueueName, true);
+            _messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(byte[]) });
+
+            FileSystemWatcher watcher = new FileSystemWatcher(FolderPath)
+            {
+                EnableRaisingEvents = true,
+                Filter = "*.zip"
+            };
+
+            watcher.Created += SendFileToQueue;
+
+            Console.WriteLine("Press Enter to exit:");
+            Console.ReadLine();
+        }
+
+        static void SendFileToQueue(object sender, FileSystemEventArgs e)
+        {
+            int retryCount = 0;
+            //try to read file
+            while (retryCount < 3)
+            {
+                try
+                {
+                    if (_messageQueue.Transactional == true)
+                    {
+                        MessageQueueTransaction myTransaction = new MessageQueueTransaction();
+                        myTransaction.Begin();
+
+                        byte[] buffer = new byte[BufferSize];
+
+                        using (var stream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                        {
+                            long remaining = stream.Length;
+                            int bytesRead;
+                            while (remaining > 0 && (bytesRead = stream.Read(buffer, 0, (int)Math.Min(remaining, BufferSize))) > 0)
+                            {
+                                byte[] arrayToSent;
+                                if (bytesRead < BufferSize)
+                                {
+                                    arrayToSent = new byte[bytesRead];
+                                    Array.Copy(buffer, arrayToSent, bytesRead);
+                                }
+                                else
+                                {
+                                    arrayToSent = buffer;
+                                }
+
+                                _messageQueue.Send(arrayToSent, e.Name, myTransaction);
+
+                                remaining -= bytesRead;
+                            }
+                        }
+
+                        myTransaction.Commit();
+                        return;
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"Try #{retryCount++}");
+                    Thread.Sleep(200);
+                }
+            }
+        }
+    }
+}
---
+++ Messaging/MsmqServer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MsmqServer
+{
+    public class Program
+    {
+        private const string MessageQueueName = @".\private$\TestMessageQueue";
+        private const string FolderPath = @"C:\Users\Yurii_Kyselov\ServerFolder";
+
+        private static MessageQueue _messageQueue;
+
+        private static void Main(string[] args)
+        {
+            _messageQueue = MessageQueue.Exists(MessageQueueName) ? new MessageQueue(MessageQueueName) : MessageQueue.Create(MessageQueueName, true);
+
+            _messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(byte[]) });
+            _messageQueue.MessageReadPropertyFilter = new MessagePropertyFilter
+            {
+                IsLastInTransaction = true,
+                Label = true,
+                Body = true,
+            };
+
+            Task.Run(Worker);
+
+            Console.ReadLine();
+
+        }
+
+        static void Worker()
+        {
+            while (true)
+            {
+                using (var trans = new MessageQueueTransaction())
+                {
+                    Stream stream = null;
+                    try
+                    {
+                        var fileName = string.Empty;
+
+                        trans.Begin();
+                        Message message;
+                        while ((message = _messageQueue.Receive(trans)) != null)
+                        {
+                            if(stream == null)
+                            {
+                                fileName = message.Label;
+                                Console.WriteLine($"Start reading ({fileName})");
+                                stream = new FileStream($"{FolderPath}\\{fileName}", FileMode.Create, FileAccess.Write);
+                            }
+
+                            WriteToStream(stream, (byte[])message.Body);
+                            if (message.IsLastInTransaction)
+                            {
+                                break;
+                            }
+                        }
+
+                        Console.WriteLine($"End reading ({fileName})");
+
+                        stream.Dispose();
+                        trans.Commit();
+                    }
+                    catch (MessageQueueException e)
+                    {
+                        if (e.MessageQueueErrorCode == MessageQueueErrorCode.TransactionUsage)
+                        {
+                            Console.WriteLine("Something is wrong. Queue is not in transactional mode.");
+                        }
+
+                        trans.Abort();
+                    }
+                }
+            }
+        }
+
+        private static void WriteToStream(Stream stream, IEnumerable<byte> bytes)
+        {
+            foreach (byte @byte in bytes)
+            {
+                stream.WriteByte(@byte);
+            }
+        }
+    }
+}
